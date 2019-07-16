42d838ff8a6f84aed82fb4b143cbfc459519ceb9
Austin Gomez <hello@austingomez.me>
2019-07-16 14:34:06 -0400

    Added review endpoint on companies

--- internapi/companies/views.py
+++ internapi/companies/views.py
@@ -1,17 +1,28 @@
+from rest_framework.response import Response
+
 from companies.serializers import CompanySerializer, CompanySearchSerializer, CompanyAutoCompleteSerializer
 from companies.models import Company
 from rest_framework import viewsets
 from rest_framework import filters
-from rest_framework.decorators import action
+from base.filters import RelatedOrderingFilter
+from rest_framework.decorators import action, detail_route
 from drf_haystack.viewsets import HaystackViewSet
 from drf_haystack.filters import HaystackAutocompleteFilter
 
+from reviews.models import Review
+from reviews.serializers import ReviewSerializer
 
 class CompanyViewSet(viewsets.ModelViewSet):
     serializer_class = CompanySerializer
     queryset = Company.objects.all()
     lookup_field = 'slug'
 
+    @detail_route()
+    def reviews(self, request, slug=None):
+        company = self.get_object()
+        reviews = Review.objects.filter(company_id=company).order_by('-created_date')
+        review_serializer = ReviewSerializer(reviews, many=True)
+        return Response(review_serializer.data)
 
 class CompanySearchView(HaystackViewSet):
     index_models = [Company]
@@ -20,7 +31,6 @@ class CompanySearchView(HaystackViewSet):
 
 
 class CompanyAutocompleteSearchViewSet(HaystackViewSet):
-
     index_models = [Company]
     serializer_class = CompanyAutoCompleteSerializer
     filter_backends = [HaystackAutocompleteFilter]
--- internapi/reviews/serializers.py
+++ internapi/reviews/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from reviews.models import Review
 from companies.serializers import CompanySerializer
-from jobs.serializers import JobSerializer
+from jobs.serializers import JobSerializer, Job
 
 
 class ReviewSerializer(serializers.ModelSerializer):
