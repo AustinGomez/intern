99958447a378a69e18fee7fe64ac804c819dff56
Austin Gomez <hello@austingomez.me>
2019-07-17 16:27:29 -0400

    Added functionality for redirect on password confirmation, updated templates for email confirmation and password reset

--- internapi/internapi/settings/base_settings.py
+++ internapi/internapi/settings/base_settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -172,5 +173,9 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True
+ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = reverse_lazy('account_confirm_complete')
+ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = reverse_lazy('account_confirm_complete')
 
 DEFAULT_FROM_EMAIL = 'noreply@internbeat.com'
--- internapi/internapi/settings/development_settings.py
+++ internapi/internapi/settings/development_settings.py
@@ -3,3 +3,7 @@ from .base_settings import *
 DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# AllAuth
+ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = \
+    ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = 'http://localhost:3000/confirmed'
