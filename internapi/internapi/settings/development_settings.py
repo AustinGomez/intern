d7f9ce164a1e75e294892281f7b230087ab4f3b6
Matt Hergott <matt.c.hergott@gmail.com>
2019-07-18 11:11:39 -0400

    Update allowed hosts to accommodate Matt's dev environment (#7)
    * update allowed hosts to accomodate matts dev environment
    
    * Update mail settings

--- internapi/internapi/settings/development_settings.py
+++ internapi/internapi/settings/development_settings.py
@@ -2,4 +2,6 @@ from .base_settings import *
 
 DEBUG = True
 
-MAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+ALLOWED_HOSTS = ['192.168.0.36', 'localhost']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
