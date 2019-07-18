382f3337281b97dbe0713aee7ea97821c2800b1b
Matt Hergott <matt.c.hergott@gmail.com>
2019-07-18 11:10:00 -0400

    Update mail settings

--- internapi/internapi/settings/development_settings.py
+++ internapi/internapi/settings/development_settings.py
@@ -4,4 +4,4 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.0.36', 'localhost']
 
-MAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
