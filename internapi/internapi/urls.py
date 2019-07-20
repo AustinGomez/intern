ea39b126e3fa81103658717230bd769950b0c2d7
AustinGomez <hello@austingomez.me>
2019-07-20 11:51:23 -0400

    Removed elasticsearch (#9)

--- internapi/internapi/urls.py
+++ internapi/internapi/urls.py
@@ -3,15 +3,13 @@ from django.urls import path
 from django.conf.urls import url, include
 from rest_framework import routers
 
-from companies.views import CompanyViewSet, CompanySearchView, CompanyAutocompleteSearchViewSet
+from companies.views import CompanyViewSet
 from jobs.views import JobViewSet
 from reviews.views import ReviewViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r'companies', CompanyViewSet)
-router.register(r'search', CompanySearchView, basename="search")
-router.register(r'autocomplete', CompanyAutocompleteSearchViewSet, basename="auto")
 
 router.register(r'jobs', JobViewSet)
 router.register(r'reviews', ReviewViewSet)
