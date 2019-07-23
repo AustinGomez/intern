4739b15bd7ccfd4b362e0286807782fc3984f9cb
AustinGomez <hello@austingomez.me>
2019-07-23 17:10:32 -0400

    added db indexes

--- internapi/reviews/models.py
+++ internapi/reviews/models.py
@@ -6,16 +6,15 @@ class Review(BaseModel):
     description = models.TextField(blank=True, null=True)
     salary_in_cents = models.BigIntegerField(blank=True, null=True)
     anonymous = models.BooleanField(default=True)
-    job = models.ForeignKey(to='jobs.Job', on_delete=models.CASCADE)
-    # TODO: make this a real field.
-    user_id = models.IntegerField()
-    # user_id = models.ForeignKey(to='users.User', on_delete=models.CASCADE)
-
     overall_rating = models.FloatField(null=False, blank=False)
-    pay_period = models.CharField(max_length = 50, blank=False, null=False)
+    pay_period = models.CharField(max_length=50, blank=False, null=False)
     currency = models.CharField(max_length=5, blank=False, null=False)
     mentorship_rating = models.FloatField(blank=False, null=False)
     work_life_balance_rating = models.FloatField(blank=False, null=False)
     meaningful_work_rating = models.FloatField(blank=False, null=False)
-    company = models.ForeignKey(to="companies.Company", on_delete=models.CASCADE)
 
+    job = models.ForeignKey(db_index=True, to='jobs.Job', on_delete=models.CASCADE)
+    company = models.ForeignKey(db_index=True, to="companies.Company", on_delete=models.CASCADE)
+    # TODO: make this a real field.
+    user_id = models.IntegerField()
+    # user_id = models.ForeignKey(to='users.User', on_delete=models.CASCADE)
