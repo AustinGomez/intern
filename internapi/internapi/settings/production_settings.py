ded859e4fd7cb910f8223fbc0e839208f9a23c84
Austin Gomez <hello@austingomez.me>
2019-08-28 14:00:20 -0400

    Remove todo

--- internapi/internapi/settings/production_settings.py
+++ internapi/internapi/settings/production_settings.py
@@ -18,7 +18,6 @@ DATABASES = {
 ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = \
     ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = 'http://internbeat.com/confirmed'
 
-# TODO: set this up.
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mailgun.org'
 EMAIL_PORT = 587
