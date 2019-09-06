449761202c376445467ea47ae6b8f293448d346f
AustinGomez <hello@austingomez.me>
2019-09-06 12:43:29 -0400

    Removed company from global state

--- client/src/_state/GlobalStateProvider.js
+++ client/src/_state/GlobalStateProvider.js
@@ -2,23 +2,17 @@ import {
   reducer as userReducer,
   initialState as userInitialState
 } from "./UserState";
-import {
-  reducer as companyReducer,
-  initialState as companyInitialState
-} from "./CompanyState";
 import { mergeReducers } from "./StateUtils.js";
 import { StateProvider } from "./State";
 import React from "react";
 
 export const GlobalStateProvider = props => {
   const reducer = mergeReducers({
-    userReducer,
-    companyReducer
+    userReducer
   });
 
   const initialState = {
-    ...userInitialState,
-    ...companyInitialState
+    ...userInitialState
   };
 
   return (
